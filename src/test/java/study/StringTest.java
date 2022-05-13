4f296054586ca02eb241cb238b5191812b492bfc
nooose <hd15807@gmail.com>
2022-05-13 16:26:33 +0900

    테스트 케이스 요구사항 2 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,13 +8,23 @@ public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
+
         assertThat(actual).isEqualTo("adc");
     }
 
     @Test
     void split() {
         String[] splitStringArray = "1,2".split(",");
+
         assertThat(splitStringArray).contains("1", "2");
         assertThat(splitStringArray).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        String subStr = str.substring(1, str.length() - 1);
+
+        assertThat(subStr).isEqualTo("1,2");
     }
 }
