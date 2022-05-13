585480888e5d3202f08aef589ed489f53608e8f2
nooose <hd15807@gmail.com>
2022-05-13 16:40:57 +0900

    테스트 케이스 요구사항 3 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,7 +2,7 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -26,5 +26,24 @@ public class StringTest {
         String subStr = str.substring(1, str.length() - 1);
 
         assertThat(subStr).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        String str = "abc";
+
+        assertThat(str.charAt(0)).isEqualTo('a');
+        assertThat(str.charAt(2)).isEqualTo('c');
+
+        assertThatThrownBy(() -> {
+            str.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    str.charAt(4);
+                }).withMessageMatching("String index out of range: \\d+");
+
     }
 }
