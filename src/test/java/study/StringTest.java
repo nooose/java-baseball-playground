b900a6298911c6ed62d2631e708e1fc279a5269f
nooose <hd15807@gmail.com>
2022-05-17 10:18:55 +0900

    test: 문자열 계산 테스트코드 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,10 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
@@ -23,7 +27,7 @@ public class StringTest {
     @Test
     void substring() {
         String str = "(1,2)";
-        String subStr = str.substring(1, str.length() - 1);
+        String subStr = str.substring(1, 4);
 
         assertThat(subStr).isEqualTo("1,2");
     }
@@ -45,5 +49,66 @@ public class StringTest {
                     str.charAt(4);
                 }).withMessageMatching("String index out of range: \\d+");
 
+    }
+
+    @Test
+    void calculateString() {
+        String value = "2 + 3 * 4 / 2";
+        String[] splitStr = getSplitStr(value);
+
+        int result = calculate(splitStr);
+
+        assertThat(result).isEqualTo(10);
+    }
+
+    int calculate(String[] splitStr) {
+        for (int i = 1; i < splitStr.length; i += 2) {
+            String op = splitStr[i];
+
+            int a = Integer.parseInt(splitStr[i - 1]);
+            int b = Integer.parseInt(splitStr[i + 1]);
+
+            switch (op) {
+                case "+":
+                    splitStr[i + 1] = Integer.toString(plus(a, b));
+                    break;
+                case "-":
+                    splitStr[i + 1] = Integer.toString(minus(a, b));
+                    break;
+                case "*":
+                    splitStr[i + 1] = Integer.toString(multiply(a, b));
+                    break;
+                case "/":
+                    splitStr[i + 1] = Integer.toString(divide(a, b));
+                    break;
+            }
+        }
+
+        return Integer.parseInt(splitStr[splitStr.length - 1]);
+    }
+
+    String[] getSplitStr(String value) {
+        return value.split(" ");
+    }
+
+    int plus(int a, int b) {
+        return a + b;
+    }
+
+    int minus(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        try {
+            return a / b;
+        } catch (Exception exception) {
+            System.out.println("0으로 나눌 수 없습니다.");
+            return a / b;
+        }
     }
 }
